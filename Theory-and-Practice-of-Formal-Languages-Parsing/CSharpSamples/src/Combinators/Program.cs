02ca74985b4f467b663d4437129f6944ab25ee83
Ivan Kochurkin <kvanttt@gmail.com>
2017-09-12 13:35:57 +0300

    Added Sprache combinator sample.

---
+++ Theory-and-Practice-of-Formal-Languages-Parsing/CSharpSamples/src/Combinators/Program.cs
@@ -0,0 +1,24 @@
+﻿using Sprache;
+using System.Linq;
+using static System.Console;
+
+namespace Combinators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser<string> identifier =
+                from leading in Parse.WhiteSpace.Many()
+                from first in Parse.Letter.Once()
+                from rest in Parse.LetterOrDigit.Many()
+                from trailing in Parse.WhiteSpace.Many()
+                select new string(first.Concat(rest).ToArray());
+
+            var id = identifier.Parse(" abc123  ");
+
+            WriteLine(id);
+            ReadLine();
+        }
+    }
+}
